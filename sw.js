b359a5f94909684e5bb1ecff7ff81aa3bb306d03
julioolands <xjuliolandx@gmail.com>
2026-04-18 01:55:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-const CACHE = 'fixo-v1';
-const ASSETS = ['/', '/index.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'fixo-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/fixo-app/', '/fixo-app/index.html']))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
